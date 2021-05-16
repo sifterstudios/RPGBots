64b2b41daaa55793e32498353e8f4511b44cbf03
Tormod Liseth <tormod@sifterstudios.no>
2021-05-16 12:48:54 +0200

    Animated a 3d controller

--- Assets/Scripts/ThirdPersonMover.cs
+++ Assets/Scripts/ThirdPersonMover.cs
@@ -5,8 +5,13 @@ public class ThirdPersonMover : MonoBehaviour
     [SerializeField] float _turnSpeed = 1000f;
     [SerializeField] float _moveSpeed = 5f;
     Rigidbody _rigidbody;
+    Animator _animator;
 
-    void Awake() => _rigidbody = GetComponent<Rigidbody>();
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
@@ -18,12 +23,15 @@ public class ThirdPersonMover : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.LeftShift))
+            vertical *= 2f;
 
         var velocity = new Vector3(horizontal, 0, vertical);
         velocity *= _moveSpeed * Time.fixedDeltaTime;
         Vector3 offset = transform.rotation * velocity;
         _rigidbody.MovePosition(transform.position + offset);
 
-
+        _animator.SetFloat("Vertical", vertical, 0.1f, Time.deltaTime);
+        _animator.SetFloat("Horizontal", horizontal, 0.1f, Time.deltaTime);
     }
 }
